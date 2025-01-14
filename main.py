25cd2228e60288563f4a60583e94be18b98215ef
SokuyaMelon <habat.madik@gmail.com>
2025-01-14 16:39:57 +0100

    Added circles, added player 'triangle', added functionality to draw player every frame, added functionality to rotate player via a and d key or left arrow and right arrow keys.

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen1 = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    x = (SCREEN_WIDTH/2)
+    y = (SCREEN_HEIGHT/2)
+    player = Player(x, y)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="black")
+            
+        screen1.fill("black")
+
+        player.draw(screen1)
+        player.update(dt)
+
         pygame.display.flip()
+
+        #limt the framerate
+        dt = clock.tick(60.0)/1000
 
 if __name__ == "__main__":
     main()
